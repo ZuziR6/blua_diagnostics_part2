4bbf57795dbb80a7f52a934109a0d1e8dabfc2b7
ZuziR6 <gabrielgoliver1@gmail.com>
2026-05-21 10:56:53 -0300

    Update retriever.py

--- src/rag/retriever.py
+++ src/rag/retriever.py
@@ -1,1 +1,10 @@
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
 
+def get_retriever():
+    db = Chroma(
+        persist_directory="chroma_db",
+        embedding_function=OpenAIEmbeddings()
+    )
+
+    return db.as_retriever(search_kwargs={"k": 3})
