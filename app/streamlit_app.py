60398664c95f388153fcd3689b84bf941abd062e
ZuziR6 <gabrielgoliver1@gmail.com>
2026-05-21 10:58:25 -0300

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,1 +1,20 @@
+import streamlit as st
+from src.graph.workflow import graph
 
+st.title("BluaDiagnostics - Care Plus")
+
+user_input = st.text_input(
+    "Digite seus sintomas:"
+)
+
+if st.button("Enviar"):
+
+    result = graph.invoke({
+        "input": user_input
+    })
+
+    st.write(result)
+
+    if "docs" in result:
+        st.subheader("Documentos recuperados pelo RAG")
+        st.write(result["docs"])
