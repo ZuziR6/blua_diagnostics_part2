3313a1fdf1112745b678109f33a611f132373879
ZuziR6 <gabrielgoliver1@gmail.com>
2026-05-21 10:57:21 -0300

    Update prescription_agent.py

--- src/agents/prescription_agent.py
+++ src/agents/prescription_agent.py
@@ -1,1 +1,26 @@
 
+from langchain_openai import ChatOpenAI
+from src.tools.medication_tools import (
+    check_medication_interaction
+)
+
+llm = ChatOpenAI(model="gpt-4o-mini")
+
+def prescription_agent(user_input):
+    interaction = check_medication_interaction(
+        "ibuprofeno"
+    )
+
+    prompt = f"""
+    Você é um agente de prescrição.
+
+    Interação medicamentosa:
+    {interaction}
+
+    Pergunta:
+    {user_input}
+    """
+
+    response = llm.invoke(prompt)
+
+    return response.content
