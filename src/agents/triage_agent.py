7524c763d7dfa2fc6a8b407a9e44f2022838db3a
ZuziR6 <gabrielgoliver1@gmail.com>
2026-05-21 10:57:09 -0300

    Update triage_agent.py

--- src/agents/triage_agent.py
+++ src/agents/triage_agent.py
@@ -1,1 +1,30 @@
+from langchain_openai import ChatOpenAI
+from src.rag.retriever import get_retriever
 
+llm = ChatOpenAI(model="gpt-4o-mini")
+
+retriever = get_retriever()
+
+def triage_agent(user_input):
+    docs = retriever.invoke(user_input)
+
+    context = "\n".join(
+        [doc.page_content for doc in docs]
+    )
+
+    prompt = f"""
+    Você é um agente de triagem clínica Care Plus.
+
+    Contexto clínico:
+    {context}
+
+    Pergunta:
+    {user_input}
+    """
+
+    response = llm.invoke(prompt)
+
+    return {
+        "response": response.content,
+        "docs": context
+    }
