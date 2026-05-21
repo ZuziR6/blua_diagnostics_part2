59d083d7c985579e7d8f4ce73ef7d0ff2a4b270b
ZuziR6 <gabrielgoliver1@gmail.com>
2026-05-21 11:44:48 -0300

    Update triage_agent.py

--- src/graph/triage_agent.py
+++ src/graph/triage_agent.py
@@ -1,1 +1,21 @@
+from src.rag.retriever import get_retriever
+from langchain_openai import ChatOpenAI
 
+llm = ChatOpenAI(model="gpt-4o-mini")
+retriever = get_retriever()
+
+def triage_agent(state):
+
+    docs = retriever.invoke(state["input"])
+    state["retrieved_docs"] = [d.page_content for d in docs]
+
+    response = llm.invoke(f"""
+Paciente: {state['patient']}
+Contexto: {state['retrieved_docs']}
+Sintomas: {state['input']}
+""")
+
+    state["final_answer"] = response.content
+    state["tools_used"].append("rag")
+
+    return state
