f73ad63464aabc888ff4dddfb62a2d9661fb1550
ZuziR6 <gabrielgoliver1@gmail.com>
2026-05-21 11:45:06 -0300

    Update prescription_agent.py

--- src/graph/prescription_agent.py
+++ src/graph/prescription_agent.py
@@ -1,1 +1,17 @@
+from langchain_openai import ChatOpenAI
 
+llm = ChatOpenAI(model="gpt-4o-mini")
+
+def prescription_agent(state):
+
+    response = llm.invoke(f"""
+Você é um agente de prescrição.
+
+Paciente: {state['patient']}
+Pedido: {state['input']}
+""")
+
+    state["final_answer"] = response.content
+    state["tools_used"].append("prescription")
+
+    return state
