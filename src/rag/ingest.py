4d8519ffbf4a167b59fd62253e5ca492ec54d2bc
ZuziR6 <gabrielgoliver1@gmail.com>
2026-05-21 10:56:43 -0300

    Update ingest.py

--- src/rag/ingest.py
+++ src/rag/ingest.py
@@ -1,1 +1,21 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import DirectoryLoader
+from src.rag.vector_store import create_vector_store
 
+loader = DirectoryLoader(
+    "data/knowledge_base",
+    glob="**/*.md"
+)
+
+documents = loader.load()
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=500,
+    chunk_overlap=50
+)
+
+chunks = splitter.split_documents(documents)
+
+create_vector_store(chunks)
+
+print("Base vetorial criada.")
