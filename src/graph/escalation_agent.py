a6e554807eba2716bc3debb5e13633e16c8d90cd
ZuziR6 <gabrielgoliver1@gmail.com>
2026-05-21 11:45:18 -0300

    Update escalation_agent.py

--- src/graph/escalation_agent.py
+++ src/graph/escalation_agent.py
@@ -1,1 +1,9 @@
+def escalation_agent(state):
 
+    state["final_answer"] = (
+        "CASO GRAVE. Encaminhamento imediato para atendimento humano."
+    )
+
+    state["tools_used"].append("escalation")
+
+    return state
