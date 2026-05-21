71fbb836bb6b97f9dd139237de9a55dcadb0292d
ZuziR6 <gabrielgoliver1@gmail.com>
2026-05-21 10:55:42 -0300

    Update patient_tools.py

--- src/tools/patient_tools.py
+++ src/tools/patient_tools.py
@@ -1,1 +1,8 @@
-
+def get_patient_record():
+    return {
+        "nome": "Maria",
+        "idade": 34,
+        "historico": "Hipertensão",
+        "ultima_consulta": "03/2026 com Dr. João",
+        "medicacao": "Losartana 50mg"
+    }
