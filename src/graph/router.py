342bbd13d3423c726834be64becd5d161db4053c
ZuziR6 <gabrielgoliver1@gmail.com>
2026-05-21 20:48:40 -0300

    Update router.py

--- src/graph/router.py
+++ src/graph/router.py
@@ -1,1 +1,17 @@
+CARDIOLOGY_KEYWORDS = [
+    "dor no peito",
+    "pressão alta",
+    "hipertensão",
+    "taquicardia"
+]
 
+
+def cardiology_router(state):
+
+    question = state["question"].lower()
+
+    for keyword in CARDIOLOGY_KEYWORDS:
+        if keyword in question:
+            return "cardiology"
+
+    return "prescription"
