865ce18b220b036154d1bb7e591f1a58084239ed
ZuziR6 <gabrielgoliver1@gmail.com>
2026-05-21 21:07:51 -0300

    Update vector_store.py

--- src/rag/vector_store.py
+++ src/rag/vector_store.py
@@ -1,5 +1,5 @@
 from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings
+from langchain_ollama import ChatOllama
 
 def create_vector_store(documents):
     embeddings = OpenAIEmbeddings()
